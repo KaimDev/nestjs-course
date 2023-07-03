375d9df9053c6302f9d5f65070e5fbe6ff42efbe
KaimDev <johanedfb@gmail.com>
2023-07-03 00:06:14 -0600

    Authentication

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,12 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Task])
+    TypeOrmModule.forFeature([Task]), 
+    AuthModule
   ],
   controllers: [TasksController],
   providers: [TasksService],
